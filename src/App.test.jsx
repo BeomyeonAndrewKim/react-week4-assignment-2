3299b1ba8e4ae4d1455079f8577e8d4abb6919b5
Beomyeon <beomyeon.kim@adriel.ai>
2021-12-27 00:38:24 +0900

    fix test case for adding it

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,9 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  const { getByText } = render(<App />);
+  it('render title text correctly', () => {
+    const { getByText } = render(<App />);
 
-  expect(getByText(/Restaurants/)).not.toBeNull();
+    expect(getByText(/Restaurants/)).not.toBeNull();
+  });
 });
