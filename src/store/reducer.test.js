5440cc3e6dde8dbae9f6e41100c91d484f75e868
Beomyeon <beomyeon.kim@adriel.ai>
2022-01-03 22:14:30 +0900

    refactor reducer test

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -28,51 +28,57 @@ describe('reducer', () => {
     });
   });
 
-  it('initialize restaurants list', () => {
-    const initialState = {
-      restaurants: [],
-    };
-    const state = reducer(initialState, setRestaurants(restaurants));
+  context('setRestaurants', () => {
+    it('reset restaurants state', () => {
+      const initialState = {
+        restaurants: [],
+      };
+      const state = reducer(initialState, setRestaurants(restaurants));
 
-    expect(state.restaurants).not.toHaveLength(0);
+      expect(state.restaurants).not.toHaveLength(0);
+    });
   });
 
-  it('change restaruant form', () => {
-    const initialState = {
-      restaurant: {
-        name: '이름',
-        category: '분류',
-        address: '주소',
-      },
-    };
-    const state = reducer(initialState, changeRestaurantField({
-      name: 'name',
-      value: '마법사주방',
-    }));
+  context('changeRestaurantField', () => {
+    it('change restaurant form', () => {
+      const initialState = {
+        restaurant: {
+          name: '이름',
+          category: '분류',
+          address: '주소',
+        },
+      };
+      const state = reducer(initialState, changeRestaurantField({
+        name: 'name',
+        value: '마법사주방',
+      }));
 
-    expect(state.restaurant.name).toBe('마법사주방');
+      expect(state.restaurant.name).toBe('마법사주방');
+    });
   });
 
-  it('appends restaurant into restaurants and clear restaurant form', () => {
-    const initialState = {
-      newId: 101,
-      restaurants: [],
-      restaurant: {
-        name: '마법사주방',
-        category: '이탈리안',
-        address: '서울시 강남구 역삼동',
-      },
-    };
+  context('addRestaurant', () => {
+    it('appends restaurant into restaurants and clear restaurant form', () => {
+      const initialState = {
+        newId: 101,
+        restaurants: [],
+        restaurant: {
+          name: '마법사주방',
+          category: '이탈리안',
+          address: '서울시 강남구 역삼동',
+        },
+      };
 
-    const state = reducer(initialState, addRestaurant());
+      const state = reducer(initialState, addRestaurant());
 
-    expect(state.restaurants).toHaveLength(1);
-    expect(state.restaurant.name).toBe('');
+      expect(state.restaurants).toHaveLength(1);
+      expect(state.restaurant.name).toBe('');
 
-    const restaurant = state.restaurants[state.restaurants.length - 1];
-    expect(restaurant.id).toBe(101);
+      const restaurant = state.restaurants[state.restaurants.length - 1];
+      expect(restaurant.id).toBe(101);
+      expect(restaurant.name).toBe('마법사주방');
 
-    expect(restaurant.name).toBe('마법사주방');
-    expect(state.newId).toBe(102);
+      expect(state.newId).toBe(102);
+    });
   });
 });
